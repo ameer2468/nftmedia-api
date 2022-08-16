f67e17525a4ae740a2c3680ee02459fa72ea2c85
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2022-08-16 03:14:25 +0300

    fetch post

---
+++ functions/posts/fetchPost/index.js
@@ -0,0 +1,44 @@
+const supabase = require("./supabase");
+
+
+exports.handler = async (event) => {
+    const postId = event.queryStringParameters.postid;
+    const userId = event.queryStringParameters.userid;
+    let response = {};
+    await Promise.all([
+        supabase.from("threads").select("*").eq("id", postId),
+        supabase.from("comments").select("*").eq("thread_id", postId),
+        supabase
+            .from("votes")
+            .select("*")
+            .eq("thread_id", postId)
+            .eq("user_id", userId),
+        supabase.from("votes").select("*").eq("thread_id", postId)
+    ]).then(([thread, comments, didUserVote, addVotes]) => {
+        const countVotes = addVotes.data.reduce(
+            (acc, vote) => {
+                return acc + vote.dir;
+            },
+            0
+        );
+        response = {
+            statusCode: 200,
+            headers: {
+                "Content-Type": "application/json",
+                'Access-Control-Allow-Origin': '*'
+            },
+            body: JSON.stringify({
+                thread: thread.data[0],
+                comments: [...comments.data],
+                didUserVote: didUserVote.data[0] || null,
+                addVotes: countVotes,
+            }),
+        }
+    }).catch((err) => {
+        response = {
+            statusCode: 400,
+            response: err.message
+        }
+    });
+    return response;
+}
