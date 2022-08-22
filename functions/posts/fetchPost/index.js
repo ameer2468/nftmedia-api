0dabb477c5ed25fcf0a8a85ad34cedf147d8b5cd
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2022-08-22 16:22:03 +0300

    wip

--- functions/posts/fetchPost/index.js
+++ functions/posts/fetchPost/index.js
@@ -1,46 +1,53 @@
 const supabase = require("./supabase");
 
-
 exports.handler = async (event) => {
-    const postId = event.queryStringParameters.postid;
-    const userId = event.queryStringParameters.userid;
-    let response = {};
-    await Promise.all([
-        supabase.from("threads").select("*").eq("id", postId),
-        supabase.from("comments").select("*").eq("thread_id", postId),
-        supabase
-            .from("votes")
-            .select("*")
-            .eq("thread_id", postId)
-            .eq("user_id", userId),
-        supabase.from("votes").select("*").eq("thread_id", postId),
-        supabase.from("comments").select("*", {count: "exact"}).eq("thread_id", postId),
-    ]).then(([thread, comments, didUserVote, addVotes, commentCount]) => {
-        const countVotes = addVotes.data.reduce(
-            (acc, vote) => {
-                return acc + vote.dir;
-            },
-            0
-        );
-        const didUserVoteForThisPost = didUserVote.data.length > 0 ? {dir: didUserVote.data[0].dir, id: didUserVote.data[0].id} : {dir: null, id: null};
-        response = {
-            statusCode: 200,
-            headers: {
-                "Content-Type": "application/json",
-                'Access-Control-Allow-Origin': '*'
-            },
-            body: JSON.stringify({
-                thread: {...thread.data[0], vote_count: countVotes,
-                    comment_count: commentCount.count,
-                    didUserVote: didUserVoteForThisPost},
-                comments: [...comments.data]
-            }),
-        }
-    }).catch((err) => {
-        response = {
-            statusCode: 400,
-            response: err.response.data.message
-        }
+  const postId = event.queryStringParameters.postid;
+  const userId = event.queryStringParameters.userid;
+  let response = {};
+  await Promise.all([
+    supabase.from("threads").select("*").eq("id", postId),
+    supabase
+      .from("comments")
+      .select("*", { count: "exact" })
+      .eq("thread_id", postId),
+    supabase
+      .from("votes")
+      .select("*")
+      .eq("thread_id", postId)
+      .eq("user_id", userId),
+    supabase.from("votes").select("*").eq("thread_id", postId),
+  ])
+    .then(([thread, comments, didUserVote, addVotes, avatar_url]) => {
+      const countVotes = addVotes.data.reduce((acc, vote) => {
+        return acc + vote.dir;
+      }, 0);
+      const didUserVoteForThisPost =
+        didUserVote.data.length > 0
+          ? { dir: didUserVote.data[0].dir, id: didUserVote.data[0].id }
+          : { dir: null, id: null };
+      response = {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          thread: {
+            ...thread.data[0],
+            user_image_url: avatar_url.data[0].avatar_url,
+            vote_count: countVotes,
+            comment_count: comments.count,
+            didUserVote: didUserVoteForThisPost,
+          },
+          comments: [...comments.data],
+        }),
+      };
+    })
+    .catch((err) => {
+      response = {
+        statusCode: 400,
+        response: err.response.data.message,
+      };
     });
-    return response;
-}
+  return response;
+};
--- functions/profile/index.js
+++ functions/profile/index.js
@@ -1,66 +1,95 @@
 const supabase = require("./supabase");
-const axios = require('axios');
-
+const axios = require("axios");
 
 exports.handler = async (event) => {
-    const user_id = event.queryStringParameters.userId;
-     let response = {};
-      await axios.all([
-            supabase
-                .from("auth")
-                .select("wallet, created_at, display_name")
-                .eq("id", user_id),
-            supabase
-                .from("threads")
-                .select("*", { count: "exact" })
-                .eq("user_id", user_id),
-            supabase
+  const user_id = event.queryStringParameters.userId;
+  const followed_by_id = event.queryStringParameters.followedBy;
+  let response = {};
+  await axios
+    .all([
+      supabase
+        .from("auth")
+        .select("wallet, created_at, display_name, id, avatar_image_url")
+        .eq("id", user_id),
+      supabase
+        .from("threads")
+        .select("*", { count: "exact" })
+        .eq("user_id", user_id),
+      supabase
+        .from("comments")
+        .select("*", { count: "exact" })
+        .eq("user_id", user_id),
+      supabase
+        .from("followers")
+        .select("*", { count: "exact" })
+        .eq("user_id", user_id),
+      supabase
+        .from("comments")
+        .select("thread_title, thread_id, id")
+        .eq("user_id", user_id),
+      supabase.from("threads").select("*").eq("user_id", user_id),
+      supabase
+        .from("followers")
+        .select("*")
+        .eq("followed_by_id", followed_by_id),
+    ])
+    .then(
+      axios.spread(
+        async (
+          user,
+          threadCount,
+          commentCount,
+          followCount,
+          comments,
+          threads,
+          followers
+        ) => {
+          const threadStats = threads.data.map(async (thread) => {
+            return {
+              ...thread,
+              comment_count: await supabase
                 .from("comments")
                 .select("*", { count: "exact" })
-                .eq("user_id",user_id),
-            supabase
-                .from("followers")
-                .select("*", { count: "exact" })
-                .eq("user_id", user_id),
-            supabase
-                .from("comments")
-                .select("thread_title, thread_id, id")
-                .eq("user_id", user_id),
-            supabase.from("threads").select("*").eq("user_id", user_id),
-            supabase.from("followers").select("*").eq("followed_by_id", user_id),
-        ]).then(axios.spread(async (user, threadCount, commentCount, followCount, comments, threads, followers) => {
-            const threadStats = threads.data.map(async (thread) => {
-                return {...thread,
-                    comment_count: await supabase.from("comments").select("*", { count: "exact" }).eq("thread_id", thread.id).then(res => res.count),
-                    vote_count: await supabase.from("votes").select("*").eq("thread_id", thread.id).then(res => res.data.reduce((acc, curr) => acc + curr.dir, 0)),
-                };
-            })
-         const data = await Promise.all(threadStats).then(res => {
-               return res;
-         })
+                .eq("thread_id", thread.id)
+                .then((res) => res.count),
+              vote_count: await supabase
+                .from("votes")
+                .select("*")
+                .eq("thread_id", thread.id)
+                .then((res) =>
+                  res.data.reduce((acc, curr) => acc + curr.dir, 0)
+                ),
+            };
+          });
+          const data = await Promise.all(threadStats).then((res) => {
+            return res;
+          });
 
-            response = {
-                statusCode: 200,
-                headers: {
-                    "Content-Type": "application/json",
-                    'Access-Control-Allow-Origin': '*'
-                },
-                body: JSON.stringify({
-                    user: {
-                        ...user.data[0],
-                        postCount: threadCount.count + commentCount.count,
-                        followerCount: followCount.count,
-                        isFollowing: followers.data.length > 0,
-                    },
-                    comments: comments.data,
-                    threads: data,
-                }),
-            }
-        })).catch((err) => {
-            response = {
-                statusCode: 400,
-                response: err.message
-            }
-        });
-        return response;
-}
+          response = {
+            statusCode: 200,
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+            },
+            body: JSON.stringify({
+              user: {
+                ...user.data[0],
+                postCount: threadCount.count + commentCount.count,
+                followerCount: followCount.count,
+                isFollowing: followers.data.length > 0,
+              },
+              comments: comments.data,
+              threads: data,
+            }),
+          };
+        }
+      )
+    )
+    .catch((err) => {
+      response = {
+        statusCode: 400,
+        response: err.message,
+      };
+    });
+  return response;
+};
