43e29cc935b1f551053cfad335b4e6380ba86741
ameer2468 <33054370+ameer2468@users.noreply.github.com>
2022-09-11 00:25:26 +0300

    'wi['

--- functions/chats/index.js
+++ functions/chats/index.js
@@ -4,8 +4,14 @@ const axios = require("axios");
 exports.handler = async (event) => {
  let response = {};
  const display_name = event.queryStringParameters.display_name;
+ const capitalFirstLetter = display_name.charAt(0).toUpperCase() + display_name.slice(1);
  const {data, error} = await supabase.from('chats').select('*');
- const filterDataToUser = JSON.parse(data).filter((chat) => chat.users.find((user) => user.display_name === display_name));
+ const updateData = data.map((chat) => {
+return {...chat, users: JSON.parse(chat.users)}
+ });
+  const filterDataToUser = updateData.filter((chat) => {
+return chat.users.some((user) => user === capitalFirstLetter);
+  });
   if (error) {
     response = {
       statusCode: 500,
@@ -14,7 +20,8 @@ exports.handler = async (event) => {
   } else {
     response = {
       statusCode: 200,
-      body: JSON.stringify(data),
+      body: JSON.stringify(filterDataToUser),
     };
   }
+  return response;
 };
